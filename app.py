8ff922e6b04c7175a29981fe256e2cc396c492b3
nishan <khatrinishan123@gmail.com>
2018-09-04 09:41:23 +0545

    new model cnn added

--- app.py
+++ app.py
@@ -8,11 +8,7 @@ from scipy import misc
 import tensorflow as tf
 from keras.models import load_model
 import pandas as pd
-import os
-import  sys
-from werkzeug.utils import secure_filename
-import cv2
-from image import classifier_cli
+import cv2 as cv
 
 
 
@@ -29,29 +25,26 @@ def index():
 def prediction():
     if request.method == 'POST':
         datas = []
-        my_data = pd.read_csv("labels_all.csv",delimiter=",")
+        label_data = pd.read_csv("labels_all.csv",delimiter=",")
         file = request.files['image']
-
         test_image = misc.imread(file, flatten=True)
 
-
+        #resize the input image for prediction
+        test_image = cv.resize(test_image, (32, 32))
         datas.append(test_image.flatten())
-        x = np.array(datas)
-        X_test = x
+        X_test = np.array(datas)
         X_test /= 255
+
         with graph.as_default():
             prediction = model.predict_classes(X_test)
             probability = model.predict_proba(X_test)
 
-        # # squeeze value from 1D array and convert to string for clean return
+        #squeeze value from 1D array
         label = int(np.squeeze(prediction))
         max_probability=np.amax(probability)
 
-        return render_template('index.html',label = my_data.iloc[label,:].values[0],probability=max_probability*100)
-
-
+        return render_template('index.html',label = label_data.iloc[label,:].values[0],probability=max_probability*100)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    classifier_cli()
